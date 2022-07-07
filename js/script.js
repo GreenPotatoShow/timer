872d3bf9005fc34b886068e4d1ad3a574f936ce5
GreenPotatoShow <rurururororo@rambler.ru>
2022-07-07 12:09:13 +0700

    Adding buttons and text inputs. Some effect after the timer ends

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
-function go() {
-             alert('Добро пожаловать в таймер!');
-             alert(document.body);
-          }
+ function timer() {
+    let sec, min,hour,time;
+    sec=+document.getElementById("second").value;
+    min=+document.getElementById("minute").value;
+    hour=+document.getElementById("hour").value;
+    time=hour*3600+min*60+sec;
+    /*while (time===undefined||time<0){
+        alert("Вы сделали что-то не так. Попробуйте ещё раз.");
+    }*/
+    document.body.style.background='red';
+    setTimeout(()=>document.body.style.background='',1000*time);
+    //setTimeout(()=>alert("Время вышло"),1000*time);
+}
+
+start.onclick = timer;
     
