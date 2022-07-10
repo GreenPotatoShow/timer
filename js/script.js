d17fc2e234e2498f7525494e3374bb54cec1ef90
GreenPotatoShow <rurururororo@rambler.ru>
2022-07-10 19:21:22 +0700

    Corrections

--- js/script.js
+++ js/script.js
@@ -1,40 +1,39 @@
  function timer() {
     if (document.getElementById("sound1").checked){
-        audio.src="js/audio/voice.mp3";
+        audio.src = "js/audio/voice.mp3";
     }
     else{
-        audio.src="js/audio/alarm.mp3";
+        audio.src = "js/audio/alarm.mp3";
     }
-    let sec, min,hour,time;
-    sec=+document.getElementById("second").value;
-    min=+document.getElementById("minute").value;
-    hour=+document.getElementById("hour").value;
-    if (checkCorrect(sec)==false||checkCorrect(min)==false||checkCorrect(sec)==false){
+    let sec, min, hour, time;
+    sec = +document.getElementById("second").value;
+    min = +document.getElementById("minute").value;
+    hour = +document.getElementById("hour").value;
+    if (!checkCorrect(sec) || !checkCorrect(min) || !checkCorrect(sec)){
         alert("Некорpектные значения. Попробуйте ещё раз");
         return;
     }
-    time=hour*3600+min*60+sec;
-    setTimeout(()=>document.body.style.backgroundImage="url('js/pictures/background2.jpg')",1000*time);
-    setTimeout(()=>sound(audio),1000*time);
+    time = hour * 3600 + min * 60 + sec;
+    setTimeout(() => document.body.style.backgroundImage = "url('js/pictures/background2.jpg')", 1000 * time);
+    setTimeout(() => sound(audio), 1000 * time);
 }
 
-function checkCorrect(x){
-    if (x===undefined||x<0){return false;}
-    return true;
+function checkCorrect(x) {
+    return (!(x === undefined) && !(x < 0));
 }
 
-function sound(audio){
+function sound(audio) {
     audio.play();
     audio.loop = true;
 }
 
-let audio=new Audio();
-let startButton=document.getElementById("start");
+let audio = new Audio();
+let startButton = document.getElementById("start");
 startButton.onclick = timer;
-let stopButton=document.getElementById("stopped");
+let stopButton = document.getElementById("stopped");
 stopButton.onclick = function(){
     audio.pause();
-    document.body.style.background='';
+    document.body.style.background = '';
 };
 
     
